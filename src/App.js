b2704ef1af65e34a81d8181e11b00cf6bf9eee97
JinHoooooou <jinho4744@naver.com>
2024-05-02 14:05:43 +0900

    JH-4: 회원 가입 페이지 추가

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ import Main from "./pages/Main";
 import Footer from "./components/Footer";
 import RequestBook from "./pages/RequestBook";
 import Login from "./pages/Login";
+import SignUpForm from "./pages/SignUpForm";
+import "./styles/signup.css"
 
 function App() {
   return (
       <BrowserRouter>
         <Header/>
         <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/requestBook" element={<RequestBook />} />
           <Route path="/" element={<Main/>}/>
           <Route path="/requestBook" element={<RequestBook/>}/>
           <Route path="/login" element={<Login/>}/>
+          <Route path="/sign-up" element={<SignUpForm/>}/>
         </Routes>
         <Footer/>
       </BrowserRouter>
