e996899d4177313ebd7a14d50ed0764d74473fe9
hankyungeun <az2282@naver.com>
2024-05-16 10:20:57 +0900

    KE-12: book 정보 가져오는 url 수정

--- src/services/book/bookAPI.js
+++ src/services/book/bookAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getBookByISBN = async (ISBN) => {
-  return await axios.get(`http://localhost:8080/api/v1/books/api/v1/books/${ISBN}`)
+  return await axios.get(`http://localhost:8080/api/v1/books/${ISBN}`)
   .then( response => {
     console.log(response.data)
     return response.data;
