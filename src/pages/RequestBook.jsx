0af22c76719df0b5afc7ed2f02c07a60ff2dbbfb
min-dro <serominsockji9n9@gmail.com>
2024-05-20 11:48:00 +0900

    MS-13: 리펙토링: bookAPI 도서 요청 API 주소 변경

--- src/pages/RequestBook.jsx
+++ src/pages/RequestBook.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
 import Button from "../components/ui/Button";
 import { createBook } from "../services/book/bookAPI";
 import { fetchBookInfo } from "../services/book/booklibraryAPI";
--- src/services/book/bookAPI.js
+++ src/services/book/bookAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const createBook = async (bookRequestDto) => {
   try {
-    const response = await axios.post(`http://localhost:8080/api/v1/books/request`, bookRequestDto);
+    const response = await axios.post(`http://localhost:8080/api/v1/books`, bookRequestDto);
     return response.data;
   } catch (error) {
     throw error;
