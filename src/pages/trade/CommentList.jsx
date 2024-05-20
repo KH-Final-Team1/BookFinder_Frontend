7223f5b824a9ef3e83114860cedd1a84b11df88b
hankyungeun <az2282@naver.com>
2024-05-20 12:18:09 +0900

    KE-16: 댓글 삭제 기능 추가

--- src/pages/trade/CommentList.jsx
+++ src/pages/trade/CommentList.jsx
@@ -1,11 +1,11 @@
 import {useEffect, useState} from "react";
-import {enrollComment, getComments} from "../../services/trade/comment";
+import { deleteComment, enrollComment, getComments } from "../../services/trade/comment";
 import Button from "../../components/ui/Button";
 
 export default function CommentList({tradeId}) {
   const [comments, setComments] = useState(null);
   const [error, setError] = useState(null);
-  const [content, setContent] = useState(null);
+  const [content, setContent] = useState("");
   const [secret, setSecret] = useState(false);
   const getSecretValue = (isChecked) => { return isChecked ? 'Y' : 'N'; };
 
@@ -31,11 +31,33 @@ export default function CommentList({tradeId}) {
     try{
       console.log(newComment);
       await enrollComment(newComment, tradeId);
-      setContent("");
-      setSecret(false);
       alert('댓글 등록을 성공하였습니다.');
+      setSecret(false);
+      setContent("");
+      const data = await getComments(tradeId);
+      setComments(data);
     } catch (error) {
       alert('댓글 등록 중 오류가 발생했습니다.');
+    }
+  };
+
+  const handleDeleteComment = async (commentId) => {
+    const result = window.confirm('댓글을 삭제하시겠습니까?');
+    if (!result) {
+      return;
+    }
+
+    try {
+      const success = await deleteComment(commentId);
+      if (success) {
+        alert('댓글 삭제를 성공하였습니다.');
+        const data = await getComments(tradeId);
+        setComments(data);
+      } else {
+        alert('댓글 삭제 중 오류가 발생했습니다.');
+      }
+    } catch (error) {
+      alert('댓글 삭제 중 오류가 발생했습니다.');
     }
   };
 
@@ -46,7 +68,7 @@ export default function CommentList({tradeId}) {
           {error && <div className="error">{error}</div>}
           {comments && comments.map(comment => {
             return (
-                <div>
+                <div key={comment.id}>
                   <div className={'comment-detail'}>
                     <tr>
                       <td className="user-name">{comment.user.nickname}</td>
@@ -54,6 +76,13 @@ export default function CommentList({tradeId}) {
                     </tr>
                     <tr>
                       <td colSpan={2} className="content">{comment.content}</td>
+                    </tr>
+                    <tr>
+                      <td colSpan={2} className="delete-comment">
+                        <img src={'https://cdn.icon-icons.com/icons2/2715/PNG/512/x_icon_172101.png'}
+                             onClick={() => handleDeleteComment(comment.id)}
+                        />
+                      </td>
                     </tr>
                   </div>
                   <hr/>
@@ -64,11 +93,12 @@ export default function CommentList({tradeId}) {
         </table>
         <div className="input-comment">
           <textarea placeholder={'댓글 내용을 입력하세요.'}
+                    value={content}
                     onChange={(event) => setContent(event.target.value)}
           ></textarea>
-          <div>
+          <div className={'select-secret'}>
             <p>비밀댓글</p>
-            <input type="checkbox" checked={secret} onChange={(event) => setSecret(event.target.value)} />
+            <input type="checkbox" checked={secret} onChange={(event) => setSecret(event.target.checked)} />
           </div>
           <Button className={'submit-button'} onClick={submitComment}>작성</Button>
         </div>
