a07f59d1bd525f1cb1a1c74efce078dcce882a15
hankyungeun <az2282@naver.com>
2024-05-22 15:25:04 +0900

    KE-29: 리팩토링: alert창 두 번 뜨는 오류 수정

--- src/components/common/PrivateRoute.jsx
+++ src/components/common/PrivateRoute.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Navigate } from 'react-router-dom';
 import isLogin from "../../utils/isLogin";
 
 const PrivateRoute = ({ element: Element, ...rest }) => {
+	const alertShown = useRef(false);
+
+	useEffect(() => {
+		if (!isLogin() && !alertShown.current) {
+			alertShown.current = true;
+			alert("로그인 후 이용 가능합니다");
+		}
+	}, []);
+
 	if (!isLogin()) {
-		alert("로그인 후 이용 가능합니다");
 		return <Navigate to="/login" />;
 	} else {
 		return <Element {...rest} />;
