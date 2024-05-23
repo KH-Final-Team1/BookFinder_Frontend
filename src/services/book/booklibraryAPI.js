1bb6af38a5b26d937e3e30c3f40de18a6ecd1e45
dasom <sommcoding@gmail.com>
2024-05-23 15:46:21 +0900

    DS-19: API GET 요청 query params 사용

--- src/services/book/booklibraryAPI.js
+++ src/services/book/booklibraryAPI.js
@@ -5,7 +5,8 @@ const AUTH_KEY = '87ee5bbc25f7e601490254d33120aa676b1fc80490d7e4c1cb0d1b04ceb70e
 
 export const fetchBookInfo = async (isbn) => {
   try {
-    const response = await axios.get(`${API_URL}/srchDtlList?authKey=${AUTH_KEY}&isbn13=${isbn}&format=json`);
+    const response = await axios.get(
+        `${API_URL}/srchDtlList?authKey=${AUTH_KEY}&isbn13=${isbn}&format=json`);
     return response.data;
   } catch (error) {
     throw error;
@@ -14,7 +15,15 @@ export const fetchBookInfo = async (isbn) => {
 
 export const getLibraryList = async (isbn, pageNo) => {
   try {
-    const response = await axios.get(`${API_URL}/libSrchByBook?authKey=${AUTH_KEY}&isbn=${isbn}&region=11&format=json&pageNo=${pageNo}`);
+    const response = await axios.get(`${API_URL}/libSrchByBook`, {
+      params: {
+        authKey: AUTH_KEY,
+        isbn: isbn,
+        region: "11",
+        format: "json",
+        pageNo: pageNo
+      }
+    });
     return response.data;
   } catch (error) {
     console.log(error)
@@ -24,7 +33,14 @@ export const getLibraryList = async (isbn, pageNo) => {
 
 export const loanBook = async (libCode, isbn) => {
   try {
-    const response = await axios.get(`${API_URL}/bookExist?authKey=${AUTH_KEY}&libCode=${libCode}&isbn13=${isbn}&format=json`);
+    const response = await axios.get(`${API_URL}/bookExist`, {
+      params: {
+        authKey: AUTH_KEY,
+        libCode: libCode,
+        isbn13: isbn,
+        format: "json"
+      }
+    })
     return response.data;
   } catch (error) {
     throw error;
